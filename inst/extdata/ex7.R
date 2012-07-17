37487afe0673558ff8f17fd47dd3a0569a6c4570
SRenan <renan.sauteraud@gmail.com>
2012-07-17 16:28:25 -0700

    Modifs to make ex7 work

--- inst/extdata/ex7.R
+++ inst/extdata/ex7.R
@@ -14,6 +14,9 @@ library(Pviz)
 ##################Peptide set construction
 mapFile<-"/home/rsautera/workspace/ex_files/smapping.csv"
 pSet1<-makePeptideSet(files=NULL,path="/home/rsautera/workspace/ex_files/RV2/",mapping.file=mapFile,use.flags=FALSE,rm.control.list=c("empty","none","JPT-","Ig","Cy","landmark"),log=TRUE,verbose=TRUE)
+#
+#data(pSet1.rda)
+#
 data(pep_hxb2)
 ndb<-convertDB(db=pep_hxb2)
 psSet1<-summarizePeptides(pSet1,summary="median",position=ndb)
@@ -29,30 +32,28 @@ load("/home/rsautera/workspace/ex_files/treatment.rda")
 #treatment<-treatment[1:198]
 treatment<-treatment[1:16]
 RV144.V<-pnSet1[,!grepl("P",treatment)]
-#RV144.P<-pnSet1[,grepl("P",treatment)]
 RV144s.V<-psSet1[,!grepl("P",treatment)]
-#RV144s.P<-psSet1[,grepl("P",treatment)]
 RV144.V.smooth<-slidingMean(pnSet1[,!grepl("P",treatment)],width=9)
-#RV144.P.smooth<-slidingMean(pnSet1[,grepl("P",treatment)],width=9)
 cutoff<-1.1
-#RV144.P.calls<-makeCalls(RV144.P.smooth,cutoff=cutoff,method="global")
 RV144.V.calls<-makeCalls(RV144.V.smooth,cutoff=cutoff,method="absolute")
 RVVcalls<-RV144.V.calls
-#RVPcalls<-RV144.P.calls
 dim(RVVcalls)<-c(1423,1)
-#dim(RVPcalls)<-c(1423,1)
 RV144.V.freq<-apply(RVVcalls,1,mean)
-#RV144.P.freq<-apply(RVPcalls,1,mean)
 
 
 #seqByClade<-apply(clade(RV144.V.smooth),2,function(x,y){y[x]},y=peptide(RV144.V.smooth))
 seqByClade<-apply(clade(RV144.V.smooth),2,function(x,y){y[x]},y=RV144.V.smooth@featureRange$aligned)
 posByClade<-apply(clade(RV144.V.smooth),2,function(x,y){y[x]},y=start(RV144.V.smooth))
 intByClade<-apply(clade(RV144.V.smooth),2,function(x,y){y[x]},y=RV144.V.freq)
-
+#
+#data(pepMicroarrayEx)
+#seqByClade<-pepMicroarrayEx$probeSeq
+#posByClade<-pepMicroarrayEx$probePos
+#intByClade<-pepMicroarrayEx$probeFreq
+#
 
 ##################Scale
-alignObj<-readAlign()
+alignObj <- readAlign(filename = system.file("extdata/alignment.fasta",package = "Pviz"))
 refScale<-alignObj[[1]]
 refSeq<-alignObj[[2]]
 
